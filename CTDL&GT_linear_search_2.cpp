ea60c56f06f2f25996f2fbed9f0b6a40749c1cd0
nguyenhoanganu <129148984+nguyenhoanganu@users.noreply.github.com>
2023-04-04 15:58:34 +0700

    Update CTDL&GT_linear_search_2.cpp

--- CTDL&GT_linear_search_2.cpp
+++ CTDL&GT_linear_search_2.cpp
@@ -1,4 +1,22 @@
-input: mảng A[] = {10, 20, 80, 30, 60, 50, 
-                     110, 100, 130, 170}
-          x = 110;
-outout:6
+#include <stdio.h> 
+  
+int search(int arr[], int n, int x) 
+{ 
+    int i; 
+    for (i = 0; i < n; i++) 
+        if (arr[i] == x) 
+            return i; 
+    return -1; 
+} 
+  
+int main(void) 
+{ 
+    int arr[] = { 2, 3, 4, 10, 40 }; 
+    int x = 10; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    int result = search(arr, n, x); 
+    (result == -1) ? printf("Element is not present in array") 
+                   : printf("Element is present at index %d", 
+                            result); 
+    return 0; 
+}
